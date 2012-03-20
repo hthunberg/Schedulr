0c4d02048694417301fc98fe358710202b0c9c07
Hans Thunberg <hans.thunberg@gmail.com>
2012-03-20 15:22:47 +0100

    Added more logging

--- Schedulr/grails-app/conf/Config.groovy
+++ Schedulr/grails-app/conf/Config.groovy
@@ -72,14 +72,12 @@ environments {
 
 // log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    
+    appenders {
+        console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    }
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+	error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
@@ -90,4 +88,6 @@ log4j = {
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
+	
+    debug  "org.callistasoftware.schedulr"
 }
